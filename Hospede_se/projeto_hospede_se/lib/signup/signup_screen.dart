963b8c30d43d3d1d4f83ae853f767cdc925ee37c
gustavo <gustavoads.millionary@gmail.com>
2021-10-05 12:08:02 -0300

    subindo tela_cadastro com alguns erros

--- Hospede_se/projeto_hospede_se/lib/signup/signup_screen.dart
+++ Hospede_se/projeto_hospede_se/lib/signup/signup_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_hospede_se/helpers/validators.dart';
+import 'package:projeto_hospede_se/models/user.dart';
 
 class SignUpPage extends StatelessWidget {
+  final ButtonStyle style = ElevatedButton.styleFrom(
+      primary: Colors.green,
+      padding: EdgeInsets.symmetric(vertical: 12),
+      textStyle: const TextStyle(
+        fontSize: 20,
+      ));
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  final User user = User();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: const Text('Criar Conta'),
         centerTitle: true,
@@ -12,35 +27,89 @@ class SignUpPage extends StatelessWidget {
       body: Center(
         child: Card(
           margin: const EdgeInsets.symmetric(horizontal: 16),
-          child: ListView(
-            padding: EdgeInsets.all(16),
-            shrinkWrap: true,
-            children: <Widget>[
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Nome Completo'),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'E-mail'),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Senha'),
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              TextFormField(
-                decoration: const InputDecoration(hintText: 'Confirmar Senha'),
-              ),
-            ],
+          child: Form(
+            key: formKey,
+            child: ListView(
+              padding: EdgeInsets.all(16),
+              shrinkWrap: true,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Nome Completo'),
+                  validator: (name) {
+                    if (name!.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (name.trim().split(' ').length <= 1)
+                      return 'Preencha seu nome completo';
+                    return null;
+                  },
+                  onSaved: (name) {},
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (email) {
+                    if (email!.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (!emailValid(email)) return 'E-mail inválido';
+                    return null;
+                  },
+                  onSaved: (email) => user.email = email,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(hintText: 'Senha'),
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass!.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (pass.length < 8)
+                      return 'Senha muito curta. Mínimo 8 caracteres';
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                TextFormField(
+                  decoration:
+                      const InputDecoration(hintText: 'Confirmar Senha'),
+                  obscureText: true,
+                  validator: (pass) {
+                    if (pass!.isEmpty)
+                      return 'Campo obrigatório';
+                    else if (pass.length < 8)
+                      return 'Senha muito curta. Mínimo 8 caracteres';
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                  style: style,
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                    }
+                  },
+                  child: const Text('Criar Conta'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<User>('user', user));
   }
 }

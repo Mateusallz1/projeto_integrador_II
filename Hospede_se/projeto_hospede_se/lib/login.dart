63f1ec7f47f3c2191582397bc0c75f352f77ee88
Aroldo Jales <aroldo_jales@hotmail.com>
2021-10-06 15:57:41 -0300

    fix(login.dart): provider and form added

--- Hospede_se/projeto_hospede_se/lib/login.dart
+++ Hospede_se/projeto_hospede_se/lib/login.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_hospede_se/models/user.dart';
+import 'package:projeto_hospede_se/models/user_manager.dart';
 import 'package:projeto_hospede_se/styles/style.dart';
-import 'package:projeto_hospede_se/registerhotel.dart';
+import 'package:projeto_hospede_se/helpers/validators.dart';
+import 'package:provider/provider.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class LoginPage extends StatelessWidget {
+  final emailController = TextEditingController();
+  final passController = TextEditingController();
 
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
       appBar: AppBar(
         actions: <Widget>[
           ElevatedButton(
@@ -23,57 +21,78 @@ class _LoginPageState extends State<LoginPage> {
               Navigator.of(context).pushNamed('/signup');
             },
             child: const Text(
-              'CRIAR CONTA',
+              'Criar Conta',
               style: TextStyle(fontSize: 14),
             ),
           )
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  'Hospede-se',
-                  style: titleText,
-                ),
-                Container(
-                  margin: const EdgeInsets.all(5),
-                  child: TextField(
-                    decoration: inputDecorationRadius('Email'),
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme: const ColorScheme.light(primary: Colors.green),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Form(
+              key: formKey,
+              child: ListView(
+                padding: const EdgeInsets.all(20),
+                shrinkWrap: true,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.all(5),
+                    child: TextFormField(
+                      controller: emailController,
+                      decoration: inputDecorationRadius('Email'),
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      validator: (email) {
+                        if (!emailValid(email!)) {
+                          return 'E-mail inválido';
+                        }
+                      },
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(5),
-                  child: TextField(
-                    decoration: inputDecorationRadius('Senha'),
-                    obscureText: true,
+                  Container(
+                    margin: const EdgeInsets.all(5),
+                    child: TextFormField(
+                      controller: passController,
+                      decoration: inputDecorationRadius('Senha'),
+                      autocorrect: false,
+                      obscureText: true,
+                      validator: (pass) {
+                        if (pass!.isEmpty || pass.length < 6) {
+                          return 'Senha inválida';
+                        }
+                      },
+                    ),
                   ),
-                ),
-                Container(
-                  margin: const EdgeInsets.all(10),
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const RegisterHotel()),
-                      );
-                    },
-                    icon: const Icon(Icons.login),
-                    label: const Text('Login'),
-                    style: elevatedButton,
+                  Container(
+                    margin: const EdgeInsets.all(5),
+                    padding: const EdgeInsets.all(5),
+                    width: MediaQuery.of(context).size.width * 0.83,
+                    height: 70,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (formKey.currentState!.validate()) {
+                          context.read<UserManager>().signIn(
+                                UserApp(
+                                    email: emailController.text,
+                                    password: passController.text),
+                              );
+                        }
+                      },
+                      child: const Text('Login'),
+                      style: elevatedButton,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ],
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }

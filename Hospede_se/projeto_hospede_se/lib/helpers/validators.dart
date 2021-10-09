48e0102fa6c79296dbd50b2724328c33caa77dad
Aroldo Jales <aroldo_jales@hotmail.com>
2021-10-09 09:36:31 -0300

    fix(validators.dart) validate name, email, password

--- Hospede_se/projeto_hospede_se/lib/helpers/validators.dart
+++ Hospede_se/projeto_hospede_se/lib/helpers/validators.dart
@@ -1,5 +1,22 @@
-bool emailValid(String email) {
-  final RegExp regex = RegExp(
-      r"^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\']+)*)|(\'.+\'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$");
-  return regex.hasMatch(email);
+class Validators {
+  static String? validateName(String name) {
+    return name.isEmpty
+        ? 'Campo obrigatório'
+        : name.trim().split(' ').length <= 1
+            ? 'Preencha seu nome completo'
+            : null;
+  }
+
+  static String? validateEmail(String email) {
+    final RegExp regex = RegExp(r"^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\']+)*)|(\'.+\'))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$");
+    return regex.hasMatch(email) ? null : 'E-mail inválido';
+  }
+
+  static String? validatePassword(String password) {
+    return password.isEmpty
+        ? 'Campo Obrigatório'
+        : password.length < 8
+            ? 'Sua senha deve ter no mínimo 8 caracteres'
+            : null;
+  }
 }

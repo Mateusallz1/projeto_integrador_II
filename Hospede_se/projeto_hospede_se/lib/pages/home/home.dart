3eba37997fe4b6d99ac49a639b6e8a1d8e03e071
Aroldo Jales <aroldo_jales@hotmail.com>
2021-10-15 14:20:39 -0300

    home test

--- Hospede_se/projeto_hospede_se/lib/pages/home/home.dart
+++ Hospede_se/projeto_hospede_se/lib/pages/home/home.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_hospede_se/services/auth_service.dart';
 import 'package:projeto_hospede_se/styles/style.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    AuthService auth = Provider.of<AuthService>(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green.shade800,
       ),
       body: Center(
-        child: Text(
-          'Home',
-          style: titleTextBlack,
+        child: Column(
+          children: [
+            Text(
+              'Home',
+              style: titleTextBlack,
+            ),
+            Text(
+              '${auth.userApp?.email}',
+              style: titleTextBlack,
+            ),
+            Text(
+              '${auth.userApp?.name}',
+              style: titleTextBlack,
+            ),
+          ],
         ),
       ),
     );

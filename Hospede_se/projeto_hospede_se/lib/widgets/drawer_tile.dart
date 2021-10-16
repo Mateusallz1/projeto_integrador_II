d0db9e787334deffb8cc74ea400c8c0c2146ad65
Mateus <trafalgarmateus@gmail.com>
2021-10-16 12:24:49 -0300

    fix(drawer_tile.dart): adicinando page manager e melhorando o design do drawer

--- Hospede_se/projeto_hospede_se/lib/widgets/drawer_tile.dart
+++ Hospede_se/projeto_hospede_se/lib/widgets/drawer_tile.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_hospede_se/pages/home/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
-  DrawerTile({required this.icondata, required this.title});
+  const DrawerTile({required this.icondata, required this.title, required this.page});
 
   final IconData icondata;
   final String title;
+  final int page;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(
-          icondata,
-          size: 32,
-          color: Colors.green,
+    final int curPage = context.watch<PageManager>().page; //pega a página atual
+
+    return InkWell(
+      onTap: (){
+
+        context.read<PageManager>().setPage(page);
+      },
+      child: SizedBox( 
+        height: 60,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              child: Icon(
+                icondata,
+                size: 32,
+                color: curPage == page ? Colors.green.shade900 : Colors.green,
+              ),
+            ),
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.green,
+              ),
+            ),
+          ],
         ),
-        Text(
-          title,
-          style: const TextStyle(
-            fontSize: 16,
-            color: Colors.green,
-          ),
-        ),
-      ],
+      ),
     );
   }
 }

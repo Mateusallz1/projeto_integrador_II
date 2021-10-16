1a7c6f3ae5713d58f8da96bb0f26cb56158058e8
Aroldo Jales <aroldo_jales@hotmail.com>
2021-10-16 10:31:24 -0300

    fix(home.dart): appbar background color

--- Hospede_se/projeto_hospede_se/lib/pages/home/home.dart
+++ Hospede_se/projeto_hospede_se/lib/pages/home/home.dart
@@ -1,40 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:projeto_hospede_se/services/auth_service.dart';
-// import 'package:projeto_hospede_se/styles/style.dart';
-// import 'package:provider/provider.dart';
-
-// class HomePage extends StatelessWidget {
-//   const HomePage({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     AuthService auth = Provider.of<AuthService>(context);
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.green.shade800,
-//       ),
-//       body: Center(
-//         child: Column(
-//           children: [
-//             Text(
-//               'Home',
-//               style: titleTextBlack,
-//             ),
-//             Text(
-//               '${auth.userApp?.email}',
-//               style: titleTextBlack,
-//             ),
-//             Text(
-//               '${auth.userApp?.name}',
-//               style: titleTextBlack,
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:projeto_hospede_se/widgets/drawer.dart';
 
@@ -50,6 +13,7 @@ class HomePage extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       children: [
         Scaffold(
+          backgroundColor: Colors.green.shade300,
           drawer: const CustomDrawer(),
           appBar: AppBar(
             title: const Text('Home'),

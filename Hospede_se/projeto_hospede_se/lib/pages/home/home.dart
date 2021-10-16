1e92ab5384536d55437d323308efdd78e14b7cea
Mateus <trafalgarmateus@gmail.com>
2021-10-16 12:21:52 -0300

    upgrade(home.dart):  Permitindo navegação entre as telas da home

--- Hospede_se/projeto_hospede_se/lib/pages/home/home.dart
+++ Hospede_se/projeto_hospede_se/lib/pages/home/home.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_hospede_se/pages/home/page_manager.dart';
 import 'package:projeto_hospede_se/widgets/drawer.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   //const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: pageController,
-      physics: const NeverScrollableScrollPhysics(),
-      children: [
-        Scaffold(
-          backgroundColor: Colors.green.shade300,
-          drawer: const CustomDrawer(),
-          appBar: AppBar(
-            title: const Text('Home'),
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+          Scaffold(
+            backgroundColor: Colors.green.shade300,
+            drawer: const CustomDrawer(),
+            appBar: AppBar(
+              backgroundColor: Colors.green.shade800,
+              title: const Text('Início'),
+            ),
           ),
-        ),
-        Container(
-          color: Colors.green,
-        ),
-        Container(
-          color: Colors.yellow,
-        ),
-        Container(
-          color: Colors.blue,
-        )
-      ],
+          Scaffold(
+            backgroundColor: Colors.green.shade300,
+            drawer: const CustomDrawer(),
+            appBar: AppBar(
+              backgroundColor: Colors.green.shade800,
+              title: const Text('Quartos'),
+            ),
+          ),
+          Scaffold(
+            backgroundColor: Colors.green.shade300,
+            drawer: const CustomDrawer(),
+            appBar: AppBar(
+              backgroundColor: Colors.green.shade800,
+              title: const Text('Estadias'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }

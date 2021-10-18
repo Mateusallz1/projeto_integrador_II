f89e9caa8541bc411442359c6693e845ec44eda6
Mateus <trafalgarmateus@gmail.com>
2021-10-18 15:09:32 -0300

    add(room_manager): criando a room_manager

---
+++ Hospede_se/projeto_hospede_se/lib/pages/rooms/room_manager.dart
@@ -0,0 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:projeto_hospede_se/models/room.dart';
+
+
+class RoomManager extends ChangeNotifier {
+
+  RoomManager() {
+    _loadAllRooms();
+  }
+
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  List<Room> _allRooms = [];
+
+  Future<void> _loadAllRooms() async {
+    final QuerySnapshot snapRoom = await firestore.collection('rooms').get();
+
+    _allRooms = snapRoom.docs.map(
+      (d) => Room.fromDocument(d)).toList();
+      
+    notifyListeners();
+  }
+}

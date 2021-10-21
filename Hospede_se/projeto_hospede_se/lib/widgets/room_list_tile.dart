c90b294d091e962aaf6633540b69b3b9aa7b94ae
Aroldo Jales <aroldo_jales@hotmail.com>
2021-10-20 22:00:36 -0300

    fix: card quarto hotel

--- Hospede_se/projeto_hospede_se/lib/widgets/room_list_tile.dart
+++ Hospede_se/projeto_hospede_se/lib/widgets/room_list_tile.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:projeto_hospede_se/models/room.dart';
-
 
 class RoomListTile extends StatelessWidget {
   RoomListTile(this.room);
@@ -10,31 +10,178 @@ class RoomListTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      elevation: 5,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4)
+        side: BorderSide(color: Colors.grey.shade900, width: 3),
+        borderRadius: BorderRadius.circular(10),
       ),
       child: Container(
-        height: 100,
-        padding: const EdgeInsets.all(8),
-        child: Row(
+        padding: const EdgeInsets.all(5),
+        child: Column(
           children: [
-            AspectRatio(
-              aspectRatio: 1,
-              child: Image.network(room.images!.first),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  Text(
-                    room.title!,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w800,
+            Container(
+              height: MediaQuery.of(context).size.height * 0.045,
+              child: Expanded(
+                child: Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(left: 7),
+                      width: MediaQuery.of(context).size.height * 0.35,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            room.title!,
+                            overflow: TextOverflow.fade,
+                            maxLines: 1,
+                            softWrap: false,
+                            style: GoogleFonts.montserrat(
+                              textStyle: const TextStyle(
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  )
+                    Expanded(
+                      child: Card(
+                        elevation: 5,
+                        //color: Colors.green.shade100,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            (room.status == true)
+                                ? Text(
+                                    'Ativo',
+                                    style: GoogleFonts.montserrat(
+                                      textStyle: TextStyle(color: Colors.green.shade900, fontSize: 18, fontWeight: FontWeight.bold),
+                                    ),
+                                  )
+                                : Text(
+                                    'Inativo',
+                                    style: GoogleFonts.montserrat(
+                                      textStyle: TextStyle(color: Colors.red.shade900, fontSize: 18, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: Row(
+                children: [
+                  const AspectRatio(
+                    aspectRatio: 1,
+                    //child: Image.network(room.images!.first),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Card(
+                            elevation: 0.5,
+                            // shape: RoundedRectangleBorder(
+                            //   side: BorderSide(color: Colors.grey.shade900, width: 2),
+                            //   borderRadius: BorderRadius.circular(5),
+                            // ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(3),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    room.description!,
+                                    overflow: TextOverflow.fade,
+                                    maxLines: 3,
+                                    softWrap: true,
+                                    style: GoogleFonts.montserrat(
+                                      textStyle: const TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Card(
+                                elevation: 3,
+                                shape: RoundedRectangleBorder(
+                                  side: BorderSide(color: Colors.grey.shade900, width: 2),
+                                  borderRadius: BorderRadius.circular(5),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Text(
+                                        'Quantidade: ${room.quantity}\n'
+                                        'Capacidade: ${room.guestCount}',
+                                        overflow: TextOverflow.fade,
+                                        maxLines: 2,
+                                        softWrap: true,
+                                        style: GoogleFonts.montserrat(
+                                          textStyle: const TextStyle(
+                                            fontSize: 15,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Card(
+                                  elevation: 3,
+                                  shape: RoundedRectangleBorder(
+                                    side: BorderSide(color: Colors.grey.shade900, width: 2),
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(5),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          '${room.price} BRL',
+                                          overflow: TextOverflow.fade,
+                                          maxLines: 1,
+                                          softWrap: false,
+                                          style: GoogleFonts.montserrat(
+                                            textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
-              )
-            )
+              ),
+            ),
           ],
         ),
       ),

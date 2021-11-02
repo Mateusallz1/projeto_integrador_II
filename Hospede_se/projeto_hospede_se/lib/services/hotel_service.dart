269e07bfe7d64bb8437eef87cfe4a0102d2939ed
Mateus <trafalgarmateus@gmail.com>
2021-11-02 10:02:17 -0300

    fix: adicionando campo de pesquisa para hoteis

--- Hospede_se/projeto_hospede_se/lib/services/hotel_service.dart
+++ Hospede_se/projeto_hospede_se/lib/services/hotel_service.dart
@@ -13,6 +13,25 @@ class HotelsProvider extends ChangeNotifier {
   String get errorMessage => _errorMessage;
   bool get hasNext => _hasNext;
 
+  String _search = '';
+
+  String get search => _search;
+  set search(String value) {
+    _search = value;
+    notifyListeners();
+  }
+
+  List<Hotel> get filteredHotel {
+    final List<Hotel> filteredHotel = [];
+
+    if (search.isEmpty) {
+      filteredHotel.addAll(hotels);
+    } else {
+      filteredHotel.addAll(hotels.where((p) => p.name!.toLowerCase().contains(search.toLowerCase())));
+    }
+    return filteredHotel;
+  }
+
   void removeHotels() {
     _hotelsSnapshot.clear();
     // ignore: avoid_print
